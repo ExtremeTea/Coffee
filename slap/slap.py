10c3af9f60a42a11bc77158a29d1996df01a2c88
ExtremeTea <34802289+ExtremeTea@users.noreply.github.com>
2018-01-20 15:34:15 -0500

    Update slap.py

--- slap/slap.py
+++ slap/slap.py
@@ -2,34 +2,6 @@ from discord.ext import commands
 import random
 import discord
 
-class smile:
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(pass_context=True)
-    async def smile(self, context, member: discord.Member):
-        """smile for everyone!"""
-        author = context.message.author.mention
-        mention = member.mention
-
-        smile = "**{0} saw a smile from {1}!**"
-
-        choices = ['https://media.giphy.com/media/8R8htEk0IgAlq/giphy.gif', 'https://media.giphy.com/media/rFfmUWVMOyKVG/giphy.gif', 'https://media.giphy.com/media/ellxlkgbPTiM0/giphy.gif', 'https://media.giphy.com/media/3lBOIbP9ghX2/giphy.gif']
-
-        image = random.choice(choices)
-
-        embed = discord.Embed(description=smile.format(mention, author), colour=discord.Colour.blue())
-        embed.set_image(url=image)
-
-        await self.bot.say(embed=embed)
-
-def setup(bot):
-    n = smile(bot)
-    bot.add_cog(n)
-from discord.ext import commands
-import random
-import discord
-
 class slap:
     def __init__(self, bot):
         self.bot = bot
